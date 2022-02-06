4105caa22189d8604d763f45ed9d8f9ef2efc4e3
Benji Lewis <benjI@xfour.co.uk>
2022-02-06 09:17:39 +0000

    Added variety to the messages

--- functions/index.js
+++ functions/index.js
@@ -19,23 +19,61 @@ exports.spentMoney = functions.region("europe-west2").https.onRequest((req, res)
 });
 
 async function sendMsg(res, merchant, amount, userID) {
-	var divAmount = (amount/100).toLocaleString(
-		"en-gb", 
-		{ minimumFractionDigits: 2 }
-	).split("-")[1];
-	var msg = "Again?! Do you really need to spend another £" + divAmount + " at " + merchant + " this month?";
+	var divAmount = convertToMoney(amount);
 	var dbuser = dbUsers.doc(userID);
+	var msg;
 	const doc = await dbuser.get();
 	if (!doc.exists) {
-		console.error("No such document!");
+		console.error("No such user!");
 	} else {
-		console.log("Document data:", doc.data());
 		var phone = doc.data().phone;
+		var currentQty = doc.data()[merchant];
+		dbuser.set({
+			[merchant]: Number(currentQty) + 1
+		}, {merge: true});
+		switch (currentQty) {
+		case 1: 
+			msg = "Fine, I'll let you off just this once. But watch it,ß we don't want to spend too much at " + merchant;
+			break;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			msg = "Again?! Do you really need to spend another £" + divAmount + " at " + merchant + " this month?";
+			break;
+		case 10:
+			msg = "10 times at " + merchant + ". I'm just disapointed in you now. ";
+			break;
+		default: 
+			msg = "How many?! You're really spending £" + divAmount + " at " + merchant + " for the " + th(currentQty) + " time this month!";
+			break;
+		}
 	}
-	console.log(phone);
 	client.messages
 		.create({body: msg, from: "+447700161601", to: phone})
 		.then(message => console.log(message.sid))
 		.then(res.sendStatus(200))
 		.catch((err) => console.error(err));
+}
+
+function convertToMoney(amount) {
+	return((amount/100).toLocaleString(
+		"en-gb", 
+		{ minimumFractionDigits: 2 }
+	).split("-")[1]);
+}
+
+function th(i) {
+	var j = i % 10,
+		k = i % 100;
+	if (j == 1 && k != 11) {
+		return i + "st";
+	}
+	if (j == 2 && k != 12) {
+		return i + "nd";
+	}
+	if (j == 3 && k != 13) {
+		return i + "rd";
+	}
+	return i + "th";
 }
